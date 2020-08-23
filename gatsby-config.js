3c84cb344c4ae3ded0279f6fb4ff45f64d293cf8
John Duong <johntanduong@gmail.com>
2020-08-22 18:09:42 -0700

    plugins added

---
+++ gatsby-config.js
@@ -0,0 +1,27 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  plugins: [`gatsby-plugin-sass`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/icon.jpg`  // FAVICON ICON HERE
+
+      },
+    },
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "../styles/index.scss"
+
+const Home = () => {
+  return (
+    <div>
+      Hello world!
+    </div>
+  )
+}
+export default Home
